f9e135f826f3bec3c441fe61a53d09cb5a063e43
Marcos Eduardo <marcosev@gmail.com>
2022-12-07 17:26:11 -0300

    m1s02 - ex 2

---
+++ M1S02 - ex 2/app.js
@@ -0,0 +1,9 @@
+const idade = Number(prompt("Fale sua idade"));
+
+if (idade <= 15)
+  alert ("você é Jovem!");
+ if  (idade >= 65)
+  alert ("você faz parte da melhor idade!");
+    else if (idade >= 16 & idade <= 64)
+  alert ("você é adulto!");
+
